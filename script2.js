5047a3dfa1a8a88b8fdf1b6d10eab6ebc4695e65
francesco mansi <f.mansi@socialwebsolutions.it>
2025-07-02 15:12:50 +0200

    Aggiunta file

---
+++ script2.js
@@ -0,0 +1,54 @@
+// console.log(1);
+// console.log(2);
+// console.log(3);
+// console.log(4);
+// console.log(5);
+// console.log(6);
+// console.log(7);
+// console.log(8);
+// console.log(9);
+// console.log(10);
+
+//for(init, condizione, iterazione){ 
+//contenuto
+//}
+
+//----
+
+for (let i = 1; i <= 100; i++) {
+    // if (i == 50) {
+    //     console.log('Ciao');
+    // } else {
+    //     console.log('Il numero che sto visualizzando è ' + i);
+    // }
+    //n1 MODULO n2 === 0 //pari
+    //n1 MODULO n2 ===1 // dispari
+    // if (i % 2 === 0) {
+    //     console.log('Il numero è Pari e sto visualizzando ' + i);
+    // } else {
+    //     console.log('Il numero è Dispari e sto visualizzando ' + i);
+    // }
+
+    // if (i % 2 === 1) {
+    //     continue; 
+    // }
+    // console.log('Il numero è Pari e sto visualizzando ' + i);
+    // console.log(i);
+    // if (i == 50) {
+    //     break;
+    // }
+    console.log(i);
+    if (i == 50) {
+        console.log(i);
+        console.log('Sono ripartito e riprendo a contare');
+    }
+    //numero primo 
+    if (i % 1 === 0 && i % i === 0) {
+        console.log('numerp rimo');
+    }
+
+
+}
+
+
+//console.log(3 % 2 === 0)
